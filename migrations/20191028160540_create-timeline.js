02f0af4414e181e246c1965fdd62479037a8bee3
BolajiOlajide <bolaji.olajide@andela.com>
2018-10-29 02:41:04 +0100

    add types for timeline

--- migrations/20191028160540_create-timeline.js
+++ migrations/20191028160540_create-timeline.js
@@ -1,10 +1,15 @@
 import logger from 'winston';
 
+// utils
+import { timelineTypes } from '../utils/common';
+
 
 exports.up = knex => knex.schema
   .createTable('Timelines', table => {
     table.uuid('id').notNullable().primary();
-    table.string('type').notNullable();
+    table.enu('type', timelineTypes).notNullable();
+    table.text('comment');
+    table.string('url');
     table.uuid('loanId').references('id').inTable('Loans');
     table.timestamps(false, true); // no need to explicitly specify `created_at` and `updated_at`
     // table.timestamp("createdAt").defaultTo(knex.fn.now());
--- utils/common.js
+++ utils/common.js
@@ -3,6 +3,8 @@ import Joi from 'joi';
 
 export const loanStatus = ['paid', 'due', 'cancelled', 'pending'];
 
+export const timelineTypes = ['comment', 'attachment', 'imageUpload'];
+
 export const unrequiredUuidValidation = Joi.string().guid({
   version: ['uuidv4']
 });
